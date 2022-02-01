93d3d4043d0a8c26f5451ba5c1018aff57844f69
Oliver Eriksson <oliver.eriksson@edu.orebro.se>
2022-02-01 13:38:31 +0100

    initial

---
+++ main.js
@@ -0,0 +1,135 @@
+let ws;
+
+let monitors = [];
+
+function updateActions() {
+	let selected = monitors.filter(a => a.selected).length;
+	
+	document.getElementById("button-url").disabled = selected !== 1;
+	document.getElementById("button-temp").disabled = selected === 0;
+	document.getElementById("button-rename").disabled = selected !== 1;
+	document.getElementById("button-swap").disabled = selected !== 2;
+	document.getElementById("button-reset").disabled = selected === 0;
+}
+
+class Monitor {
+	constructor(id, ip, def) {
+		this.id = id;
+		this.ip = ip;
+		this.default = def;
+		this.selected = false;
+		
+		this.element = document.createElement("li");
+		this.headerElement = document.createElement("header");
+		this.headerElement.textContent = this.id;
+		this.element.appendChild(this.headerElement);
+		
+		this.mainElement = document.createElement("main");
+		this.mainElement.textContent = this.ip;
+		this.element.appendChild(this.mainElement);
+		
+		this.element.addEventListener("click", () => {
+			this.selected = !this.selected;
+			this.element.classList.toggle("selected");
+			updateActions();
+		});
+		
+		document.getElementById("monitors").appendChild(this.element);
+		
+		monitors.push(this);
+	}
+	
+	setId(newId) {
+		this.id = newId;
+		this.headerElement.textContent = newId;
+	}
+	
+	sendCommand(command, def) {
+		ws.send(JSON.stringify({
+			type: "command",
+			id: this.id,
+			command: command,
+			default: def
+		}));
+	}
+	
+	rename(newId) {
+		ws.send(JSON.stringify({
+			type: "rename",
+			id: this.id,
+			newId: newId
+		}));
+		this.setId(newId);
+	}
+	
+	remove() {
+		document.getElementById("monitors").removeChild(this.element);
+		monitors.splice(monitors.indexOf(this), 1);
+	}
+}
+
+function connect() {
+	ws = new WebSocket("ws://infomedia.orebro.se:1234");
+	ws.addEventListener("open", function() {
+		ws.send(JSON.stringify({
+			pass: "hejhallåjagäradmin"
+		}));
+	});
+	ws.addEventListener("message", function(event) {
+		let data = JSON.parse(event.data);
+		if (data.type === "hello") {
+			data.monitors.forEach(function(monitor) {
+				new Monitor(monitor.id, monitor.ip, monitor.default);
+			});
+		} else if (data.type === "join") {
+			new Monitor(data.id, data.ip, data.default);
+		} else if (data.type === "leave") {
+			monitors.find(a => a.id === data.id).remove();
+		} else if (data.type === "data") {
+			console.log(data.id, data.data);
+		}
+	});
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+	connect();
+	
+	document.getElementById("button-url").addEventListener("click", function() {
+		let monitor = monitors.find(a => a.selected);
+		let url = prompt("Ange URL", monitor.default.slice(6, -2));
+		if (!url) return;
+		
+		monitor.sendCommand("goto(\"" + url + "\")", true);
+	});
+	document.getElementById("button-temp").addEventListener("click", function() {
+		let url = prompt("Ange temporär URL");
+		if (!url) return;
+		monitors.forEach(monitor => {
+			if (!monitor.selected) return;
+			monitor.sendCommand("goto(\"" + url + "\")", false);
+		});
+	});
+	document.getElementById("button-rename").addEventListener("click", function() {
+		let monitor = monitors.find(a => a.selected);
+		let name = prompt("Ange nytt namn", monitor.id);
+		if (!name) return;
+		monitor.rename(name);
+	});
+	document.getElementById("button-swap").addEventListener("click", function() {
+		let selected = monitors.filter(a => a.selected);
+		ws.send(JSON.stringify({
+			type: "swap",
+			a: selected[0].id,
+			b: selected[1].id
+		}));
+		let temp = selected[0].id;
+		selected[0].setId(selected[1].id);
+		selected[1].setId(temp);
+	});
+	document.getElementById("button-reset").addEventListener("click", function() {
+		monitors.forEach(monitor => {
+			if (!monitor.selected) return;
+			monitor.sendCommand("location.reload();", false);
+		});
+	});
+});
---
+++ server/main.js
@@ -0,0 +1,181 @@
+﻿const ws = require("ws");
+const https = require("https");
+const fs = require("fs");
+
+const adminpass = "hejhallåjagäradmin";
+const monitorpass = "monitor";
+
+//const httpsServer = new https.createServer({
+//	cert: fs.readFileSync("/etc/letsencrypt/live/daydun.com/fullchain.pem"),
+//	key: fs.readFileSync("/etc/letsencrypt/live/daydun.com/privkey.pem")
+//});
+let server = new ws.Server({
+//	server: httpsServer
+	port: 1234
+});
+
+let defaults = JSON.parse(fs.readFileSync("defaults.json"));
+
+let monitors = [];
+let admins = [];
+
+class Client {
+	constructor(socket, ip) {
+		this.type = "none";
+		//this.ip = ip;
+		this.ip = "N/A";
+		
+		this.socket = socket;
+		socket.on("message", (data) => {
+			this.onMessage(data);
+		});
+		socket.on("close", () => {
+			this.onClose(socket);
+		});
+	}
+	
+	onMessage(data) {
+		data = JSON.parse(data);
+		if (this.type === "none") {
+			if (data.pass === monitorpass) {
+				this.setMonitor(data.id, data.ip);
+			} else if (data.pass === adminpass) {
+				this.setAdmin();
+			} else {
+				this.socket.close();
+			}
+			return;
+		} else if (this.type === "monitor") {
+			if (data.type === "data") {
+				admins.forEach(admin => {
+					admin.socket.send(JSON.stringify({
+						type: "data",
+						id: this.id,
+						data: data.data
+					}));
+				});
+			}
+		} else if (this.type === "admin") {
+			if (data.type === "command") {
+				monitors.find(a => a.id === data.id).socket.send(JSON.stringify({
+					type: "command",
+					command: data.command
+				}));
+				if (data.default === true) {
+					defaults.monitors[data.id] = data.command;
+					fs.writeFileSync("defaults.json", JSON.stringify(defaults));
+				}
+			} else if (data.type === "rename") {
+				let monitor = monitors.find(a => a.id === data.id);
+				monitor.id = data.newId;
+				monitor.socket.send(JSON.stringify({
+					type: "rename",
+					id: data.newId
+				}));
+				if (data.id in defaults.monitors) {
+					let cmd = defaults.monitors[data.id];
+					delete defaults.monitors[data.id];
+					defaults.monitors[data.newId] = cmd;
+					fs.writeFileSync("defaults.json", JSON.stringify(defaults));
+				}
+			} else if (data.type === "swap") {
+				let a = monitors.find(a => a.id === data.a);
+				let b = monitors.find(a => a.id === data.b);
+				
+				a.id = data.b;
+				b.id = data.a;
+				
+				a.socket.send(JSON.stringify({
+					type: "rename",
+					id: data.b
+				}));
+				a.socket.send(JSON.stringify({
+					type: "command",
+					command: defaults.monitors[data.b]
+				}));
+				b.socket.send(JSON.stringify({
+					type: "rename",
+					id: data.a
+				}));
+				b.socket.send(JSON.stringify({
+					type: "command",
+					command: defaults.monitors[data.a]
+				}));
+			}
+		}
+	}
+	
+	setMonitor(id, ip) {
+		this.type = "monitor";
+		this.id = id || "monitor-" + (Object.keys(defaults.monitors).length + 1);
+		this.ip = ip;
+		this.socket.send(JSON.stringify({
+			type: "hello",
+			id: this.id,
+			default: defaults.monitors[this.id]
+		}));
+		monitors.push(this);
+		
+		admins.forEach(admin => {
+			admin.socket.send(JSON.stringify({
+				type: "join",
+				id: this.id,
+				ip: this.ip,
+				default: defaults.monitors[this.id]
+			}));
+		});
+	}
+	
+	setAdmin() {
+		this.type = "admin";
+		this.socket.send(JSON.stringify({
+			type: "hello",
+			monitors: monitors.map(monitor => {
+				return {
+					id: monitor.id,
+					ip: monitor.ip,
+					default: defaults.monitors[monitor.id]
+				};
+			})
+		}));
+		admins.push(this);
+	}
+	
+	onClose() {
+		if (this.type === "monitor") {
+			monitors.splice(monitors.indexOf(this), 1);
+			admins.forEach(admin => {
+				admin.socket.send(JSON.stringify({
+					type: "leave",
+					id: this.id
+				}));
+			});
+		} else if (this.type === "admin") {
+			admins.splice(admins.indexOf(this), 1);
+		}
+	}
+}
+
+server.on("connection", function(socket, request) {
+	socket.isAlive = true;
+	socket.on("pong", function() {
+		socket.isAlive = true;
+	});
+	
+	let ip = request.socket.remoteAddress;
+	
+	let client = new Client(socket, ip);
+});
+
+setInterval(function() {
+	server.clients.forEach(function each(socket) {
+		if (socket.isAlive === false) return socket.terminate();
+		if (socket.readyState !== ws.OPEN) return;
+
+		socket.isAlive = false;
+		socket.ping();
+	});
+}, 30000);
+
+//httpsServer.listen(42069);
+server.on("listening", () => console.log("start"));
